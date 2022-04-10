741e197c63758049c1a3871e012703804f6e01ae
Lukas <m@hler.eu>
2022-04-10 17:56:59 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,371 @@
+__author__ = "Lukas Mahler"
+__version__ = "0.0.0"
+__date__ = "10.04.2022"
+__email__ = "m@hler.eu"
+__status__ = "Development"
+
+import re
+import os
+import json
+import time
+import glob
+import random
+import lxml.html
+from tqdm import tqdm
+from pathlib import Path
+from datetime import timedelta
+import selenium.common.exceptions
+
+# Custom
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+# Self
+from src import util
+
+
+def get_matches():
+    print("[*] Getting latest match data")
+
+    driver_class = util.ChromeDriver()
+    driver = driver_class.driver
+
+    url = f"https://steamcommunity.com/profiles/{config['steam_id']}/gcpd/730?tab=matchhistorycompetitive"
+
+    if config['cookie'] == "":
+        config['cookie'] = input("Please input your steamLoginSecure cookie: \n-> ")
+
+    cookies = {'steamLoginSecure': config['cookie']}
+
+    driver.get("https://steamcommunity.com/")
+    for name, value in cookies.items():
+        driver.add_cookie({'name': name, 'value': value})
+    time.sleep(1)
+
+    driver.get(url)
+
+    if not WebDriverWait(driver, 10).until(ec.element_to_be_clickable((By.ID, "load_more_button"))):
+        print("[Err] 'steamLoginSecure' cookie is probably expired")
+        exit(1)
+
+    last_loaded = get_last_match_data()
+
+    if not last_loaded:
+        print("[*] Looks like it's your first time using csgo-match-history, trying to load all games")
+
+    i = 1
+    while True:
+
+        print(f"\r[*] Loading matches [~{i * 8}]", end='', flush=True)
+
+        # Check if we need to load more
+        if last_loaded:
+            found_last_loaded = driver.find_elements_by_xpath(f"//*[contains(text(), '{last_loaded}')]")
+            if found_last_loaded:
+                print("\n[*] All new matches loaded")
+                break
+
+        try:
+            # element_load_more = WebDriverWait(driver, 30).until(
+            #    ec.presence_of_element_located((By.ID, "load_more_button")))
+
+            element_load_more = WebDriverWait(driver, 15).until(
+                ec.element_to_be_clickable((By.ID, "load_more_button"))
+            )
+            element_load_more.click()
+            time.sleep(random.random())
+
+        except selenium.common.exceptions.ElementClickInterceptedException:
+            print("\n[Err] Couldn't get all games due to a steam error, try again later")
+            exit(1)
+
+        except selenium.common.exceptions.TimeoutException:
+            print("\n[*] Probably all matches loaded")
+            break
+
+        i += 1
+
+    print("[*] Getting page source")
+    time.sleep(3)
+    html = driver.page_source
+    # print(html)
+    # input("-> ")
+
+    # html = requests.get(url, cookies=cookies, headers=headers).content
+    doc = lxml.html.fromstring(html)
+
+    # print(lxml.html.tostring(doc))
+    # print(doc.xpath('//*[@id="personaldata_elements_container"]/table')[0])
+    # print(doc.xpath('//*[@id="personaldata_elements_container"]/table/tbody'))
+
+    table_history = doc.xpath('//*[@id="personaldata_elements_container"]/table/tbody')[0]
+    xml = [match for match in table_history.xpath("./tr")]
+    xml.pop(0)  # Remove the headline
+
+    save_xml_to_disk(xml)
+
+
+def get_last_match_data():
+    if os.path.exists("./xml"):
+        latest = os.listdir("./xml")[-1]
+        latest = latest[:-4]  # remove .xml
+        latest = latest.split("_")
+        xtime = latest[1]
+        latest[1] = ':'.join(xtime[i:i+2] for i in range(0, len(xtime), 2))
+        return ' '.join(latest)
+    else:
+        return None
+
+
+def get_valid_filename(s):
+    s = str(s).strip().replace(' ', '_')
+    return re.sub(r'(?u)[^-\w.]', '', s)
+
+
+def save_xml_to_disk(matches):
+    Path("./xml").mkdir(parents=True, exist_ok=True)
+
+    i = 0
+    for match in tqdm(matches, unit_scale=True, ncols=50):
+        i += 1
+        try:
+            game_date = match.xpath(".//td[1]/table/tbody/tr[2]/td")[0].text_content().strip()
+            clean_game_date = get_valid_filename(game_date)
+
+            with open(f"./xml/{clean_game_date}.xml", 'wb') as f:
+                f.write(lxml.html.tostring(match))
+
+        except Exception as e:
+            print(e)
+            continue
+
+
+def download_demo(url=None, matchid=None, outcomeid=None, token=None):
+    Path("./demos").mkdir(parents=True, exist_ok=True)
+
+    # url = 'http://replay190.valve.net/730/003540319049349071083_1642207433.dem.bz2'  # Example
+
+    if url:
+
+        # Check if we haven't downloaded the demo yet
+        filename = url.split("/")[-1]
+        if os.path.exists(f"./demos/{filename}"):
+            # print("[*] Demo already downloaded")
+            return
+        else:
+            # We didn't so start the download
+            util.download_file(url, "./demos/")
+            print("[*] Finished downloading demo")
+
+
+def check_matches():
+    print("[*] Checking all match .xml files")
+    Path("./json").mkdir(parents=True, exist_ok=True)
+
+    for match_xml in glob.glob("./xml/*.xml"):
+        with open(match_xml, 'r') as xmlf:
+            with open(f"./json/{os.path.basename(xmlf.name)[:-4]}.json", 'w') as jsonf:
+                json.dump(format_matchinfo(xmlf), jsonf, indent=4)
+
+
+def check_winning(match_score, player_index):
+
+    if match_score == "15:15":
+        outcome = "Draw"
+    else:
+        if player_index > 5:
+            side = "right"
+        else:
+            side = "left"
+
+        score = match_score.split(":")
+        if score[0] == "16" and side == "left":
+            outcome = "Win"
+        elif score[1] == "16" and side == "right":
+            outcome = "Win"
+        else:
+            outcome = "Lose"
+
+    # print(f"[DEBUG] {match_score} / {player_index} / {side} / {outcome}")
+
+    return outcome
+
+
+def format_matchinfo(xmlf):
+
+    matchinfo = {}
+    match = lxml.html.parse(xmlf)
+
+    # General Match Info
+    inner_left_tds = match.xpath('.//table[@class="csgo_scoreboard_inner_left"]//td')
+    match_map = inner_left_tds[0].text_content().replace("Competitive", "").strip()
+    match_time_qued = inner_left_tds[3].text_content().strip().split(" ")[-1]
+    match_time_played = inner_left_tds[4].text_content().strip().split(" ")[-1]
+    match_score = match.xpath('.//td[@class="csgo_scoreboard_score"]')[0].text_content().strip().replace(" ", "")
+
+    if len(inner_left_tds[-1].xpath('.//a')) > 0:
+        url = inner_left_tds[-1].xpath('.//a')[0].get("href")
+        download_demo(url=url)
+
+    matchinfo_json = {
+        'xmap': match_map,
+        'score': match_score,
+        'time_que': match_time_qued,
+        'time_played': match_time_played
+    }
+
+    matchinfo['general'] = matchinfo_json
+
+    # Player Info
+    inner_right_trs = match.xpath('.//table[@class="csgo_scoreboard_inner_right"]//tr')
+    inner_right_trs.pop(0)  # Remove Headings
+    inner_right_trs.pop(5)  # Remove Score
+
+    playerinfos = {}
+    player_index = 1
+    for playerinfo in inner_right_trs:
+        steam_id = playerinfo.xpath('.//a')[0].get("href").split("/")[-1]
+
+        # Check if i was on losing or winning side
+        if steam_id == "sorryvirgin":
+            outcome = check_winning(match_score, player_index)
+            matchinfo_json['outcome'] = outcome
+            matchinfo['general'] = matchinfo_json
+
+        ping = playerinfo.xpath('.//td')[1].text_content().strip()
+        kills = playerinfo.xpath('.//td')[2].text_content().strip()
+        assists = playerinfo.xpath('.//td')[3].text_content().strip()
+        death = playerinfo.xpath('.//td')[4].text_content().strip()
+        hsp = playerinfo.xpath('.//td')[6].text_content().strip()
+        score = playerinfo.xpath('.//td')[7].text_content().strip()
+
+        single_playerinfo_json = {
+            'ping': ping,
+            'kills': kills,
+            'assists': assists,
+            'death': death,
+            'hs%': hsp,
+            'score': score
+        }
+        playerinfos[steam_id] = single_playerinfo_json
+
+        matchinfo['players'] = playerinfos
+        player_index += 1
+
+    return matchinfo
+
+
+def summarize():
+
+    stats_map = {}
+    stats_overall = {'wins': 0, 'loses': 0, 'draws': 0, 'time_que': timedelta(), 'time_played': timedelta()}
+    average_que_time = None
+    average_play_time = None
+
+    longest_que_time = timedelta()
+    longest_play_time = timedelta()
+    shortest_que_time = timedelta(days=1)
+    shortest_play_time = timedelta(days=1)
+
+    json_files = glob.glob("./json/*.json")  # [-100:] # last 100 only
+    n_matches = len(json_files)
+    print(f"[*] Summarizing data from [{n_matches}] matches")
+    for match_json in json_files:
+        with open(match_json, 'r') as jf:
+            data = json.load(jf)
+
+            matchinfo = data['general']
+            xmap = matchinfo['xmap']
+
+            if xmap not in stats_map:
+                stats_map[xmap] = {
+                    'wins': 0,
+                    'loses': 0,
+                    'draws': 0,
+                    'time_que': 0,
+                    'time_played': 0
+                }
+
+            if matchinfo['outcome'] == "Win":
+                stats_overall['wins'] += 1
+                stats_map[xmap]['wins'] += 1
+            elif matchinfo['outcome'] == "Lose":
+                stats_overall['loses'] += 1
+                stats_map[xmap]['loses'] += 1
+            elif matchinfo['outcome'] == "Draw":
+                stats_overall['draws'] += 1
+                stats_map[xmap]['draws'] += 1
+
+            m, s = matchinfo['time_que'].split(":")
+            que_timedelta = timedelta(minutes=int(m), seconds=int(s))
+            m, s = matchinfo['time_played'].split(":")
+            played_timedelta = timedelta(minutes=int(m), seconds=int(s))
+
+            # longest / shortest
+            if que_timedelta > longest_que_time:
+                longest_que_time = que_timedelta
+
+            if que_timedelta < shortest_que_time:
+                shortest_que_time = que_timedelta
+
+            if played_timedelta > longest_play_time:
+                longest_play_time = played_timedelta
+
+            if played_timedelta < shortest_play_time:
+                shortest_play_time = played_timedelta
+
+            stats_overall['time_que'] += que_timedelta
+            stats_overall['time_played'] += played_timedelta
+
+    # Calculate overall winrate
+    stats_overall['winrate'] = int(stats_overall['wins'] / (n_matches - stats_overall['draws']) * 100)
+
+    # Calculate overall que and play time average
+    stats_overall['time_que_average'] = stats_overall['time_que'] / n_matches
+    stats_overall['time_played_average'] = stats_overall['time_played'] / n_matches
+
+    # Calculate winrate per map
+    for xmap in stats_map:
+        n_map_matches = stats_map[xmap]['wins'] + stats_map[xmap]['loses']  # stats_map[xmap]['draws']
+        stats_map[xmap]['winrate'] = int((stats_map[xmap]['wins'] / n_map_matches) * 100) # noqa
+
+    util.newline()
+    print(f" |-------------|---W---L---D-|-Win%-|")
+    for xmap in stats_map:
+        print(f" |_ {xmap:10s} | "
+              f"{stats_map[xmap]['wins']:3d} "
+              f"{stats_map[xmap]['loses']:3d} "
+              f"{stats_map[xmap]['draws']:3d} | "
+              f"{stats_map[xmap]['winrate']:3d}% |")
+    print(f" |----------------------------------|\n |_ Total      | "
+          f"{stats_overall['wins']:3d} "
+          f"{stats_overall['loses']:3d} "
+          f"{stats_overall['draws']:3d} | "
+          f"{stats_overall['winrate']:3d}% |")
+
+    util.newline()
+    print(util.format_single_stat("Total que time", stats_overall['time_que']))
+    print(util.format_single_stat("Average que time", stats_overall['time_que_average']))
+    print(util.format_single_stat("Longest que time", longest_que_time))
+    print(util.format_single_stat("Shortest que time", shortest_que_time))
+    util.newline()
+    print(util.format_single_stat("Total play time", stats_overall['time_played']))
+    print(util.format_single_stat("Average play time", stats_overall['time_played_average']))
+    print(util.format_single_stat("Longest play time", longest_play_time))
+    print(util.format_single_stat("Shortest play time", shortest_play_time))
+
+
+def main():
+
+    # Load toml config
+    global config
+    config = util.getConf("prod.toml")
+
+    get_matches()
+    check_matches()
+    summarize()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/util.py
@@ -0,0 +1,179 @@
+"""
+TBD
+"""
+
+__author__ = "Lukas Mahler"
+__version__ = "0.0.0"
+__date__ = "10.04.2022"
+__email__ = "m@hler.eu"
+__status__ = "Development"
+
+
+# Default
+import math
+import shutil
+import zipfile
+import os.path
+import requests
+from pathlib import Path
+from datetime import timedelta
+
+# Custom
+import toml
+from tqdm import tqdm
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class ChromeDriver:
+
+    def __init__(self):
+
+        self.chrome_driver_path = self.getPath()
+        self.chrome_options = self.setOptions()
+
+        self.driver = webdriver.Chrome(options=self.chrome_options, executable_path=self.chrome_driver_path)
+
+        self.browser_version = self.driver.capabilities['browserVersion']
+        self.driver_version = self.driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0]
+
+        if self.browser_version[0:2] != self.driver_version[0:2]:
+            print("[*] Updating chromedriver.exe, please wait")
+            self.driver.close()
+            self.driver.quit()
+
+            self.getNew(self.chrome_driver_path)  # update our outdated driver
+            self.driver = webdriver.Chrome(options=self.chrome_options, executable_path=self.chrome_driver_path)
+
+    def getPath(self):
+        """
+            find a current 'chromedriver.exe'
+        """
+
+        driver_path = fr"{os.path.dirname(os.path.realpath(__file__))}\chromedriver.exe"
+        if os.path.isfile(driver_path):
+            return driver_path
+        else:
+            # print("[DEBUG] Chromedriver wasn't found in current working directory")
+            pass
+
+        driver_path = os.environ["temp"] + r"\Google\Chrome\Driver\chromedriver.exe"
+        if os.path.isfile(driver_path):
+            return driver_path
+        else:
+            # print(r"[DEBUG] Chromedriver wasn't found under %PROGRAMFILES(x86)%\Google\Chrome\driver")
+            pass
+
+        print("[*] Couldn't find a Chromedriver, downloading a new one")
+        self.getNew(driver_path)
+
+        return driver_path
+
+    @staticmethod
+    def setOptions():
+        # Headless Chrome
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--window-size=1920x1080")
+        chrome_options.add_argument("--log-level=3")  # Fatal
+        chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        chrome_options.add_argument('--User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                                    ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36')
+
+        return chrome_options
+
+    @staticmethod
+    def getNew(driver_path):
+
+        driver_path = Path(driver_path)
+
+        if driver_path.is_file():
+            os.remove(driver_path)  # Remove old chromedriver.exe
+
+        # Get the latest chrome driver version number
+        url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+        version_number = requests.get(url).text
+
+        # build the download url
+        url = f"https://chromedriver.storage.googleapis.com/{version_number}/chromedriver_win32.zip"
+
+        # download the zip file using the url built above
+        driver_path.parent.mkdir(parents=True, exist_ok=True)
+        driver_zip = download_file(url, dest=driver_path.as_posix())
+
+        # extract the zip file
+        with zipfile.ZipFile(driver_zip, 'r') as zipf:
+            zipf.extractall(path=driver_path.parent.as_posix())  # you can specify the destination folder path here
+
+        # delete the zip file downloaded above
+        os.remove(driver_zip)
+
+
+def getConf(fname):
+    """
+    """
+    if fname.endswith(".toml"):
+        if os.path.isfile(fname):
+            try:
+                config = toml.load(fname)
+                checkConf(config)
+                return config
+            except ValueError as e:
+                print(f"The provided '.toml' is probably invalid, returned error:\n{e}")
+                exit(1)
+        else:
+            print(f"Couldn't locate the '.toml' file [{fname}].")
+            print("Creating a new '.toml' file from template, please edit and restart.")
+            shutil.copy("src/template.toml", fname)
+            exit(1)
+    else:
+        print(f"The provided config file [{fname}] is not a '.toml' file.")
+        print("Creating a new '.toml' file from template, please edit and restart.")
+        shutil.copy("src/template.toml", "prod.toml")
+        exit(1)
+
+
+def checkConf(config):
+    """
+    TODO check if keys exist
+    """
+    pass
+
+
+def format_single_stat(tx, stat):
+
+    if isinstance(stat, timedelta):
+        # Round up timedeltas, see https://stackoverflow.com/a/60976512/5593051
+        stat = timedelta(seconds=math.ceil(stat.total_seconds()))
+
+    return f"{str(tx):20s}: {str(stat):10s}"
+
+
+def newline(n=1):
+    for i in range(0, n):
+        print("")
+
+
+def download_file(url, dest='./'):
+
+    filename = url.split("/")[-1]
+    destination = dest + filename
+    response = requests.get(url, stream=True)
+    total_size_in_bytes = int(response.headers.get('content-length', 0))
+    block_size = 1024  # 1 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True, ncols=50)
+
+    with open(destination, "wb") as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("[Err] Something went wrong")
+        exit(1)
+
+    return destination
+
+
+if __name__ == "__main__":
+    exit()
